528d2e85a4b01817732f4dd88d1d53f59e6fd79d
Administrator <admin@example.com>
2017-09-07 12:46:28 +0900

    Add env to node service

--- src/create_node.sh
+++ src/create_node.sh
@@ -9,15 +9,15 @@ read -p "$*"
 
 ## Fetch GC settings
 # project and zone
-project=$YOUR_PROJECT_ID
+project=api-project-7872450353
 zone=asia-northeast1-c
 # CoreOS release channel
 channel=stable
 # worker instance type
 # worker_machine_type=f1-micro
-worker_machine_type=n1-standard-1
+worker_machine_type=g1-small
 
-instance_name=gc-cluster-1
+instance_name=gc-node-1
 
 # get the latest full image name
 image=$(gcloud compute images list --project=$project | grep -v grep | grep coreos-$channel | awk {'print $1'})
@@ -25,9 +25,9 @@ image=$(gcloud compute images list --project=$project | grep -v grep | grep core
 
 # create cluster instance
 gcloud compute instances create $instance_name --project=$project --image=$image \
- --image-project=coreos-cloud --boot-disk-size=100 --zone=$zone \
+ --image-project=coreos-cloud --boot-disk-size=50 --zone=$zone \
  --machine-type=$worker_machine_type --metadata-from-file \
- user-data=cloud-config/cluster.yaml --can-ip-forward --tags $instance_name,gc-cluster
+ user-data=cloud-config/node.yaml --can-ip-forward --tags $instance_name,gc-cluster
 
 echo " "
 echo "Setup has finished !!!"
